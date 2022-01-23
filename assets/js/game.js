79b1ebb603d6419d544643b987e241b6be6388c2
Albert Rodriguez <alb.rodriguez7@gmail.com>
2022-01-22 19:31:39 -0800

    practice commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,19 @@
-var playerName = window.prompt("What is your robot's name?");
+let playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+
+// You can also log multiple values at once like this
+
+
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+console.log(playerName, playerAttack, playerHealth, enemyName);
+
+let fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
 
 fight();
